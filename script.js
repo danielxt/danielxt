5eb3afd8f3980af3fa9c5d1f241f8fdb964acf2d
= <='danieltok77@gmail.com>
2021-07-25 15:20:43 +0800

    foundation

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("navMenu").innerHTML =
+"<ul style='list-style-type:none; margin:0; padding:0;'>"+
+ '<li><a href="index.html">Home</a></li>'+
+ '<li><a href="services.html">Services</a></li>'+
+ '<li><a href="about.html">About</a></li>'+
+'</ul>';
